72e1221a61f5fe7dc821086863a339bdb42428dc
j-kennethh <johnkennethagpaoa@gmail.com>
2026-01-21 00:03:27 +0800

    Implement initial skeletal version

---
+++ src/main/java/Pixel.java
@@ -0,0 +1,12 @@
+public class Pixel {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Pixel");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
