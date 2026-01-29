9d5fbfba72c6813d3acde7851eee3eb806dfe606
j-kennethh <johnkennethagpaoa@gmail.com>
2026-01-30 01:02:42 +0800

    Refactor code to use Task class

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -1,44 +1,47 @@
 import java.util.Scanner;
 
 public class Pixel {
-    public static void main(String[] args) {
-        String horizontalLine = "____________________________________________________________";
+    public static String horizontalLine = "____________________________________________________________";
 
-        int count = 0;
-        String[] tasks = new String[100];
-
-        String line;
-        Scanner in = new Scanner(System.in);
-
-        // Hello Message
+    public static void printHello() {
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm Pixel");
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
+    }
 
-        // Main Loop
+    public static void printBye() {
+        System.out.println(horizontalLine);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+
+    public static void main(String[] args) {
+        Task[] tasks = new Task[100];
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        printHello();
+
         line = in.nextLine();
         while (!line.equals("bye")) {
             if (line.equals("list")) {
                 System.out.println(horizontalLine);
                 System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < count; i++) {
-                    System.out.println(i+1 + ". [ ] " + tasks[i]);
+                for (int i = 0; i < Task.getCount(); i++) {
+                    System.out.println(tasks[i].getId() + ". [ ] " + tasks[i].getName());
                 }
                 System.out.println(horizontalLine);
             } else {
-                tasks[count] = line;
-                count++;
+                Task newTask = new Task(line);
+                tasks[Task.getCount() - 1] = newTask;
                 System.out.println(horizontalLine);
-                System.out.println("added: " + line);
+                System.out.println("added: " + newTask.getName());
                 System.out.println(horizontalLine);
             }
             line = in.nextLine();
         }
 
-        // Bye Message
-        System.out.println(horizontalLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        printBye();
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,33 @@
 public class Task {
-    private String name;
+    private final int id;
+    private final String name;
     private boolean isDone;
+    private static int count = 0;
 
+    public Task(String name) {
+        count++;
+        this.id = count;
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public static int getCount() {
+        return count;
+    }
 }
